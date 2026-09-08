d5424fff0115fe9946ca5333c007bc00460a1150
mike-box <2876768308@qq.com>
2026-09-08 12:37:13 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 hexo gen
+# 先清理并重新生成，确保文件完整
 git add -A
-git commit -m "modify by mike meng"
+git commit -m "update"
 git push origin master
-git subtree push --prefix=public origin gh-pages
+# 强制重新构建 subtree 历史
+git subtree split --prefix=public -b gh-pages-temp
+git push origin gh-pages-temp:gh-pages --force
+git branch -D gh-pages-temp
